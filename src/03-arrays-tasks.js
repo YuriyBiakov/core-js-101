708d7feb88a4cd74b3d10d1b7c14805fc6a49d86
Yuriy Biakov <gbyackov@gmail.com>
2024-10-24 00:00:07 +0300

    feat: add solutions

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -240,8 +240,8 @@ function toCsvText(arr) {
  *   [ 0, 1, 2, 3, 4, 5 ] => [ 0, 1, 4, 9, 16, 25 ]
  *   [ 10, 100, -1 ]      => [ 100, 10000, 1 ]
  */
-function toArrayOfSquares(/* arr */) {
-  throw new Error('Not implemented');
+function toArrayOfSquares(arr) {
+  return arr.map((el) => el ** 2);
 }
 
 
@@ -259,8 +259,13 @@ function toArrayOfSquares(/* arr */) {
  *   [ 0, 0, 0, 0, 0]         => [ 0, 0, 0, 0, 0]
  *   [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ] => [ 1, 3, 6, 10, 15, 21, 28, 36, 45, 55 ]
  */
-function getMovingSum(/* arr */) {
-  throw new Error('Not implemented');
+function getMovingSum(arr) {
+  let sum = 0;
+  const result = arr.map((el) => {
+    sum += el;
+    return sum;
+  });
+  return result;
 }
 
 /**
@@ -274,8 +279,17 @@ function getMovingSum(/* arr */) {
  * [ 'a', 'b', 'c' , null ]  => [ "b", null ]
  * [ "a" ] => []
  */
-function getSecondItems(/* arr */) {
-  throw new Error('Not implemented');
+function getSecondItems(arr) {
+  const result = [];
+  let index = 1;
+  arr.map((el) => {
+    if (index % 2 === 0) {
+      result.push(el);
+    }
+    index += 1;
+    return 0;
+  });
+  return result;
 }
 
 
@@ -293,8 +307,17 @@ function getSecondItems(/* arr */) {
  *  [ 'a', 'b', 'c', null ] => [ 'a', 'b','b', 'c','c','c',  null,null,null,null ]
  *  [ 1,2,3,4,5 ] => [ 1, 2,2, 3,3,3, 4,4,4,4, 5,5,5,5,5 ]
  */
-function propagateItemsByPositionIndex(/* arr */) {
-  throw new Error('Not implemented');
+function propagateItemsByPositionIndex(arr) {
+  const result = [];
+  let index = 1;
+  arr.map((el) => {
+    const firstFillIndex = result.length;
+    result.length += index;
+    result.fill(el, firstFillIndex);
+    index += 1;
+    return 0;
+  });
+  return result;
 }
 
 
@@ -311,8 +334,11 @@ function propagateItemsByPositionIndex(/* arr */) {
  *   [ 1,2,3,4,5,6,7,8,9,10 ] => [ 10, 9, 8 ]
  *   [ 10, 10, 10, 10 ] => [ 10, 10, 10 ]
  */
-function get3TopItems(/* arr */) {
-  throw new Error('Not implemented');
+function get3TopItems(arr) {
+  let result = arr.slice(0);
+  result = result.sort((a, b) => b - a);
+  if (result.length > 3) result = result.slice(0, 3);
+  return result;
 }
 
 
@@ -329,8 +355,13 @@ function get3TopItems(/* arr */) {
  *   [ null, 1, 'elephant' ] => 1
  *   [ 1, '2' ] => 1
  */
-function getPositivesCount(/* arr */) {
-  throw new Error('Not implemented');
+function getPositivesCount(arr) {
+  let result = 0;
+  arr.map((el) => {
+    if (typeof el === 'number' && el > 0) result += 1;
+    return 0;
+  });
+  return result;
 }
 
 /**
@@ -362,8 +393,13 @@ function sortDigitNamesByNumericOrder(/* arr */) {
  *   [ -1, 1, -1, 1 ]      => 0
  *   [ 1, 10, 100, 1000 ]  => 1111
  */
-function getItemsSum(/* arr */) {
-  throw new Error('Not implemented');
+function getItemsSum(arr) {
+  let result = 0;
+  arr.map((el) => {
+    result += el;
+    return 0;
+  });
+  return result;
 }
 
 /**
@@ -378,8 +414,13 @@ function getItemsSum(/* arr */) {
  *  [ -1, 'false', null, 0 ] => 2
  *  [ null, undefined, NaN, false, 0, '' ]  => 6
  */
-function getFalsyValuesCount(/* arr */) {
-  throw new Error('Not implemented');
+function getFalsyValuesCount(arr) {
+  let falsyCounter = 0;
+  arr.map((el) => {
+    if (!el) falsyCounter += 1;
+    return 0;
+  });
+  return falsyCounter;
 }
 
 /**
@@ -396,8 +437,13 @@ function getFalsyValuesCount(/* arr */) {
  *    [ null, undefined, null ], null => 2
  *    [ true, 0, 1, 'true' ], true => 1
  */
-function findAllOccurrences(/* arr, item */) {
-  throw new Error('Not implemented');
+function findAllOccurrences(arr, item) {
+  let occurrences = 0;
+  arr.map((el) => {
+    if (el === item) occurrences += 1;
+    return 0;
+  });
+  return occurrences;
 }
 
 /**
@@ -496,8 +542,15 @@ function getIntervalArray(/* start, end */) {
  *   [ 'a', 'a', 'a', 'a' ]  => [ 'a' ]
  *   [ 1, 1, 2, 2, 3, 3, 4, 4] => [ 1, 2, 3, 4]
  */
-function distinct(/* arr */) {
-  throw new Error('Not implemented');
+function distinct(arr) {
+  const result = [];
+  arr.map((el) => {
+    if (result.indexOf(el) === -1) {
+      result.push(el);
+    }
+    return 0;
+  });
+  return result;
 }
 
 /**
