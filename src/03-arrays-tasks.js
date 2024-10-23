9f51c624809abc7c64187d8c7286c60252fcfccf
unknown <gbyackov@gmail.com>
2024-10-23 17:41:29 +0300

    feat: add 34s solution

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -111,8 +111,13 @@ function getArrayOfStrings(arr) {
  *    [ 1, 2, 3, 4, 5, 'false' ]         => [ 1, 2, 3, 4, 5, 'false' ]
  *    [ false, 0, NaN, '', undefined ]   => [ ]
  */
-function removeFalsyValues(/* arr */) {
-  throw new Error('Not implemented');
+function removeFalsyValues(arr) {
+  const result = [];
+  arr.map((el) => {
+    if (Boolean(el) !== false) result.push(el);
+    return 0;
+  });
+  return result;
 }
 
 /**
